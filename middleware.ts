a3cd172ee78603ac3f5783ebc6a65fac1f844352
Name <daniprojects808@gmail.com>
2026-02-25 10:46:05 +0000

    deployment fix

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,5 @@
-import { updateSession } from "./supabase/middleware";
-import { type NextRequest } from "next/server";
-
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
-}
+import { createServerClient } from "@supabase/ssr";
+import { type NextRequest, NextResponse } from "next/server";
 
 export const config = {
   matcher: [
@@ -18,3 +14,65 @@ export const config = {
     "/((?!_next/static|_next/image|favicon.ico|public|api/payments/webhook).*)",
   ],
 };
+
+export async function middleware(request: NextRequest) {
+  try {
+    // Create an unmodified response
+    let response = NextResponse.next({
+      request: {
+        headers: request.headers,
+      },
+    });
+
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return request.cookies.getAll().map(({ name, value }) => ({
+              name,
+              value,
+            }));
+          },
+          setAll(cookiesToSet) {
+            cookiesToSet.forEach(({ name, value, options }) => {
+              request.cookies.set(name, value);
+              response = NextResponse.next({
+                request: {
+                  headers: request.headers,
+                },
+              });
+              response.cookies.set(name, value, options);
+            });
+          },
+        },
+      }
+    );
+
+    // This will refresh session if expired - required for Server Components
+    const {
+      data: { user },
+      error,
+    } = await supabase.auth.getUser();
+
+    // Protected routes
+    if (request.nextUrl.pathname.startsWith("/dashboard") && error) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+
+    if (request.nextUrl.pathname === "/" && !error) {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+
+    return response;
+  } catch (e) {
+    // If you are here, a Supabase client could not be created!
+    // This is likely because you have not set up environment variables.
+    return NextResponse.next({
+      request: {
+        headers: request.headers,
+      },
+    });
+  }
+}
